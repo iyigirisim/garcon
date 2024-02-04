2c1f5a236c0aefcc5c3a3869d0e15fdb8d5e2fdc
abdullahkaraman <abdullahkaramaniletisim@gmail.com>
2024-02-04 22:05:25 +0300

    about page

---
+++ app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold">About</h1>
+      <p className="text-lg">This is the about page.</p>
+    </div>
+  );
+}
